c190a88a60fe2cdfb821fd22ef1ad7ce8c63c045
Rob Maxwell <rpmaxwell144@gmail.com>
2019-07-29 11:26:32 -0500

    test package

---
+++ sample-pkg/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name='hi',
+    version='0.0.1',
+    author='rob',
+    author_email='rpmaxwell144@gmail.com',
+    description="do libraries from git work like I think they do?",
+    py_modules=setuptools.find_packages(),
+    url='https://github.com/rpmaxwell/mini-projects/sample-pkg',
+)
